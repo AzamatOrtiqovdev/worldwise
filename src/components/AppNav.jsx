0b0ca3690874b9e6efe62729a18e30bdc8ebfd08
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2024-09-19 14:56:35 +0500

    Nested Routes and Index Route

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom"
 import {nav} from "./AppNav.module.css"
 
 function AppNav() {
       return (
             <nav className={nav}>
-                  App navigation
+                  <ul>
+                        <li>
+                              <NavLink to="cities">Cities</NavLink>
+                        </li>
+                        <li>
+                              <NavLink to="countries">Countries</NavLink>
+                        </li>
+                  </ul>
             </nav>
       )
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,21 @@
+import { Outlet } from "react-router-dom"
+import styles from "./Sidebar.module.css"
+import Logo from "./Logo"
+import AppNav from "./AppNav"
+
 function SideBar() {
       return (
-            <div>
-                  
+            <div className={styles.sidebar}>
+                  <Logo />
+                  <AppNav />
+
+                  <Outlet />
+
+                  <footer className={styles.footer}>
+                        <p className={styles.copyright}>
+                              &copy; Copyright {new Date().getFullYear()} by the World Wide Web Ltd.
+                        </p>
+                  </footer>
             </div>
       )
 }
